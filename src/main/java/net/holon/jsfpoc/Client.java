01f963557675ea47013126ecbe4682823599716d
Jacek Chmielewski <jchmielewski5@gmail.com>
2012-01-11 13:07:32 +0100

    templates

--- src/main/java/net/holon/jsfpoc/Client.java
+++ src/main/java/net/holon/jsfpoc/Client.java
@@ -1,12 +1,17 @@
 package net.holon.jsfpoc;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 
 @ManagedBean
 @ViewScoped
-public class Client {
+public class Client implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
 	private String client = "";
 	
 	public void submit() {
